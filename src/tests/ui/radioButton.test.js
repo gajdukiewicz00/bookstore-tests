973187f84079fbc2b92852456de8853d18ea7b83
gajdukiewicz00 <UL0266567@edu.uni.lodz.pl>
2024-12-25 22:45:20 +0100

    Update ci.yml and add tests: button.test.js checkbox.test.js radioButton.test.js

---
+++ src/tests/ui/radioButton.test.js
@@ -0,0 +1,19 @@
+const logger = require('../../utils/logger');
+import {test,expect} from "@playwright/test";
+
+test.describe('Radio Button Test', ()=>{
+    test('User can select a radio button', async ({page}) =>{
+        logger.info('Navigating to Radio Button page');
+        await page.goto(`${process.env.BASE_URL}/radio-button`, { waitUntil: 'networkidle' });
+
+        logger.info('Selecting "Yes" radio button');
+        await page.click('text=Yes');
+
+        logger.info('Validating selection');
+        const resultText = await page.locator('.text-success').textContent();
+        console.log('Radio button result:',resultText);
+        expect(resultText).toBe('Yes');
+
+        logger.info('Test completed successfully')
+    })
+})
